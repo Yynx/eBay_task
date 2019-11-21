06aa3570bdf7ddd3fb52299fbfcdd8399e526706
Yynx <47715709+Yynx@users.noreply.github.com>
2019-11-21 13:51:51 +0000

    Attempt using Pug to style formSent page

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const port = 3000;
 const axios = require('axios').default;
 const cors = require('cors')
+const pug = require('pug')
 
 
 app.use(cors());
@@ -12,19 +13,25 @@ app.use(express.static('views'));   // allows you to serve images, CSS files, an
 app.use(express.urlencoded()); // allows us to process the text data submitted via the POST method (the processed form data is available on the body property of the request object)
 app.engine('html', require('ejs').renderFile); //this is an extension required to avoid html render error. needed for making a project with multiple pages
 
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => res.send("index"));        // GET request to home page route, responds with index page
 
 app.post('/formSent', (req, res) => {
     // res.render("formSent.html");
+
     const name = req.body.userID;
     const item = req.body.itemName;
     const price = req.body.itemPrice;
     const description = req.body.itemDescription;
 
-    res.send(`Hi ${name}. You are selling ${item} for £${price}. Here is a description of your item: ${description}`);
+    res.render("formSent.pug", {name:name, item:item, price:price, description:description});
     // console.log(req.body); 
     // res.send(req.body);
 });
 
+app.get('/formSent', (req, res) => { 
+    res.render("formSent.html");
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));  
