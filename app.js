e81108d05e59182d6cc0edd0dbd787f4fceac624
Yynx <47715709+Yynx@users.noreply.github.com>
2019-11-21 11:58:32 +0000

    Add form action='/handler' and method='post' to form

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const cors = require('cors')
 app.use(cors());
 app.use(express.static('public'));  // allows you to serve images, CSS files, and JavaScript files in a directory named public
 app.use(express.static('views'));   // allows you to serve images, CSS files, and JavaScript files in a directory named views
-app.use(express.urlencoded()); //allows us to extract info from a html (parse URL-encoded bodies)
+app.use(express.urlencoded()); // allows us to process the text data submitted via the POST method (the processed form data is available on the body property of the request object)
 app.engine('html', require('ejs').renderFile); //this is an extension required to avoid html render error. needed for making a project with multiple pages
 
 app.get('/', (req, res) => res.send("index"));        // GET request to home page route, responds with index page
