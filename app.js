aad5d19392c4ba6135e178d1eed80cb2c758a96b
Yynx <47715709+Yynx@users.noreply.github.com>
2019-11-21 15:32:31 +0000

    Uninstalled Pug and replaced all Pug code with EJS

--- app.js
+++ app.js
@@ -4,16 +4,14 @@ const app = express();
 const port = 3000;
 const axios = require('axios').default;
 const cors = require('cors')
-const pug = require('pug')
+const ejs = require ('ejs')
 
 
 app.use(cors());
 app.use(express.static('public'));  // allows you to serve images, CSS files, and JavaScript files in a directory named public
 app.use(express.static('views'));   // allows you to serve images, CSS files, and JavaScript files in a directory named views
 app.use(express.urlencoded()); // allows us to process the text data submitted via the POST method (the processed form data is available on the body property of the request object)
-// app.engine('html', require('ejs').renderFile); //this is an extension required to avoid html render error. needed for making a project with multiple pages
-
-app.set('view engine', 'pug'); // required to use Pug (npm install pug --save)
+app.engine('html', require('ejs').renderFile); //this is an extension required to avoid html render error. needed for making a project with multiple pages
 
 app.get('/', (req, res) => res.send("index"));        // GET request to home page route, responds with index page
 
@@ -24,7 +22,8 @@ app.post('/formSent', (req, res) => {
     const price = req.body.itemPrice;
     const description = req.body.itemDescription;
 
-    res.render("formSent.pug", {name:name, item:item, price:price, description:description});
+    res.render("formSent.ejs", {name:name, item:item, price:price, description:description});
+
     // console.log(req.body); 
     // res.send(req.body);
 });
