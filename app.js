86d21e137d311afa68858095818008641369c9cc
Yynx <47715709+Yynx@users.noreply.github.com>
2019-11-21 11:50:51 +0000

    Add urlencoded and ejs lines

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const cors = require('cors')
 app.use(cors());
 app.use(express.static('public'));  // allows you to serve images, CSS files, and JavaScript files in a directory named public
 app.use(express.static('views'));   // allows you to serve images, CSS files, and JavaScript files in a directory named views
-
+app.use(express.urlencoded()); //allows us to extract info from a html (parse URL-encoded bodies)
+app.engine('html', require('ejs').renderFile); //this is an extension required to avoid html render error. needed for making a project with multiple pages
 
 app.get('/', (req, res) => res.send("index"));        // GET request to home page route, responds with index page
 
